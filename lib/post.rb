17af70b649bb3eb8b65a5db8d548830d7c157039
Roberto Livi <robertomlivi@outlook.com>
2020-02-07 17:07:03 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+class Post
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end  
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def title
+    @title
+  end
+  
+  def author_name
+    self.author == nil ? nil : self.author.name
+  end
+  
+  
+end
