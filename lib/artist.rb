92f04b9414e4e07c340dd2b4c58f594b1142d2c6
Roberto Livi <robertomlivi@outlook.com>
2020-02-07 16:07:45 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  
+attr_accessor :name, :songs
+
+@@song_count = 0
+
+def initialize(name)
+  @name = name
+end
+
+def songs
+  Song.all.select {|song| song.artist == self}
+end
+  
+def add_song(song)
+  song.artist = self
+  @@song_count += 1
+end
+
+def add_song_by_name(song_name)
+  s = Song.new(song_name)
+  add_song(s)
+end
+
+def self.song_count
+  self.all
+end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,33 @@
+class Post
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end  
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def title
+    @title
+  end
+  
+  def author_name
+    if @@all.count == 0
+      nil
+  else
+    self.author.name
+  end
+  end
+  
+  
+end
